d19ab0c37fd209dc3d5026e7ef706307f2eb042b
Shubham Shrikant Bawner <wurtzreaxnwittigreaxn@gmail.com>
2023-02-11 22:54:05 +0530

    react stable component but no functionality

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import About from "./components/About.jsx";
+import Home from "./components/Home.jsx";
+import Records from "./components/Records.jsx";
+import Sidenav from "./components/Sidenav.jsx";
+
+function App() {
+  return (
+    <div className="row g-2">
+      <div className="col-3"><Sidenav/></div>
+      <div className="col-9">
+     <Home/>
+      <Records/>
+     <About/> 
+     </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+// import Sound_visual from './ReactMicModule';
+// import Sidenav from './Sidenav'
+import styles from "./home.module.css";
+
+export default function Home() {
+  return (
+    <div className="col">
+      <div className={styles.containers}>
+        <div className={styles.previewCont}></div>
+        <div className={styles.action_container}>
+          <div className={styles.audio}>
+            {/* <Sound_visual /> */}
+            </div>
+          <div className={styles.action}>
+            <div class={styles.btn_group}>
+              <button >Start Recording</button>
+              <button >Stop Recording</button>
+              <button >Extract PPT</button>
+              <button >Extract Transcripted file</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
