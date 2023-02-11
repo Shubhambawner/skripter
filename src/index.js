6a6cea5c65ff31ef13212014615affda24a117d0
Shubham Shrikant Bawner <wurtzreaxnwittigreaxn@gmail.com>
2023-02-11 20:40:51 +0530

    unstable:react integratiuon

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './materialize.css'
+// import './style.css'
+import Home from './Home.jsx';
+
+
+
+ReactDOM.render(<Home />, document.getElementById('root'))
